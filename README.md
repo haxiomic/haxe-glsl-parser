a7f11784e82f871e1f72a6ef08ba71a682f7e09d
haxiomic <haxiomic@gmail.com>
2015-03-02 16:16:32 +0000

    First commit, working tokenizer

---
+++ README.md
@@ -0,0 +1,10 @@
+GLSL ES 1.0 Tokenizer and Parser
+-----
+
+####Tokenizer completed and produces tokens conforming to the spec outlined here [opengles_shading_language.pdf](https://www.khronos.org/files/opengles_shading_language.pdf) with a few additional tokens:
+	- BLOCK_COMMENT
+	- LINE_COMMENT
+	- PREPROCESSOR
+	- WHITESPACE
+
+####Parser in development
---
+++ tests/fuzz/Main.hx
@@ -0,0 +1,21 @@
+import glslparser.Tokenizer;
+
+class Main{
+
+	static function main(){
+		var input = "";
+		//input = sys.io.File.getContent('test.glsl');
+
+		//fuzz
+		var chars = ['e', 'E', '+', '-', '.', '_', '=', '^', '/' ,' ', ' ', ' ', '0x', '00', '\\', ' asm', ' if ', ' while ', ' struct ', '{', '}', '\n'];
+		for (i in 0...10) chars.push(Std.string(i));
+
+		for(n in 0...1000){
+			var i = Math.floor(Math.random()*chars.length);
+			input += chars[i];
+		}
+
+		var tokens = Tokenizer.tokenize(input);
+	}
+
+}
