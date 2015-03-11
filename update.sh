2a47a41fe953ff15bc745dd0f39e788c3341ee09
haxiomic <haxiomic@gmail.com>
2015-03-11 20:39:09 +0000

    Progress on update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,12 @@
+# reset any changes
 git reset --hard HEAD
+
+# rebuild demo
 git checkout master
 cd tests/parser/demo/
 make
+cd ../../../
+
+# copy demo files to this branch
 git checkout gh-pages
 git checkout master tests/parser/demo/bin
