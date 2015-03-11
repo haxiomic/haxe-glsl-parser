29f8d0b31452c053aa64266da08f9dbf2e80d4ef
haxiomic <haxiomic@gmail.com>
2015-03-11 20:46:13 +0000

    Finished update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,22 @@
+#!/bin/bash
+
+DEMO_DIR=tests/parser/demo/
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+#enter directory of the script
+cd $DIR
+
 # reset any changes
 git reset --hard HEAD
 
 # rebuild demo
 git checkout master
-cd tests/parser/demo/
+cd $DEMO_DIR
 make
-cd ../../../
+cd $DIR
 
-# copy demo files to this branch
+# # copy demo files to this branch
 git checkout gh-pages
 git checkout master tests/parser/demo/bin
+
+mv tests/parser/demo/bin/* ./
