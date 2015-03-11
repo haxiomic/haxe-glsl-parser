b989bde64677d3c222cb00aae3b139e6614e857a
haxiomic <haxiomic@gmail.com>
2015-03-11 21:14:23 +0000

    update.sh now uses try/catch pattern

--- update.sh
+++ update.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
+{
 
-DEMO_DIR=tests/parser/demo/
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+	DEMO_DIR=tests/parser/demo/
+	DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-#enter directory of the script
-cd $DIR
+	#enter directory of the script
+	cd $DIR &&
 
-# reset any changes
-git reset --hard HEAD
+	# reset any changes
+	# git reset --hard HEAD
 
-# rebuild demo
-git checkout master
-cd $DEMO_DIR
-make
-cd $DIR
+	# rebuild demo
+	git checkout master &&
+	cd $DEMO_DIR &&
+	make &&
+	cd $DIR &&
 
-# # copy demo files to this branch
-git checkout gh-pages
-git checkout master $DEMO_DIR/bin
+	# # copy demo files to this branch
+	git checkout gh-pages &&
+	git checkout master $DEMO_DIR &&
 
-rsync -a $DEMO_DIR/bin/* ./
+	rsync -a $DEMO_DIR/bin/* ./ &&
 
-rm -rf $DEMO_DIR
+	rm -rf $DEMO_DIR
+
+} || {
+	echo "Update failed"
+}
